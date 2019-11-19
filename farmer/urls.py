9760e508e4b33440cb543b4218e70654f0711a18
Kelvin Ndisi <kelvindisi@gmail.com>
2019-11-19 17:30:33 +0300

    created staff app account manager

--- farmer/urls.py
+++ farmer/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "farmer"
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('factories/', views.factories, name="factories")
 ]
--- farmer/views.py
+++ farmer/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.views import generic
 
 def index(request):
     return render(request, 'farmer/home.html')
+
+def factories(request):
+    return render(request, 'farmer/factories.html')
