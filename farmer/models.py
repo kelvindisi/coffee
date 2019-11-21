5fa58d1e774c07a61ff8ddc023ea05549807a975
Kelvin Ndisi <kelvindisi@gmail.com>
2019-11-21 13:56:53 +0300

    new database models and few changes to staff layout

--- farmer/models.py
+++ farmer/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from staff.models import Factory
 
-# Create your models here.
+
+class NotificationMessage(models.Model):
+    farmer = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=300, verbose_name="title")
+    message = models.TextField(verbose_name="body")
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="message_sender")
+
+    def __str__(self):
+        return f"{self.farmer.username} Message"
+
+
+class Product(models.Model):
+    farmer = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, verbose_name="coffee")
+    price_per_kg = models.IntegerField(default=0, verbose_name="price")
+    date_delivered = models.DateField()
+    factory = models.OneToOneField(Factory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.farmer.username} - {self.quantity}Kgs"
--- staff/models.py
+++ staff/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Factory(models.Model):
@@ -14,3 +15,35 @@ class Factory(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.email}"
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.balance}"
+
+    def get_absolute_url(self):
+        pass
+
+
+class Transaction(models.Model):
+    transaction_types = [
+        ('deposit', 'Deposit'),
+        ('cancel', 'Cancel'),
+        ('correction', 'Correction')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+    initiated_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="accountant")
+    transaction_type = models.CharField(
+        max_length=20, choices=transaction_types)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} - {self.transaction_type}"
+
+    def get_absolute_url(self):
+        pass
