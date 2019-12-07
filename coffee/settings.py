ee26ffbe6fb9812b03bc8e54128d4e6d797d7f76
Kelvin Ndisi <kelvindisi@yahoo.com>
2019-12-08 01:06:59 +0300

    new proile

--- coffee/settings.py
+++ coffee/settings.py
@@ -149,6 +149,6 @@ LOGOUT_REDIRECT_URL = 'account:login'
 LOGIN_URL = 'user:login'
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
