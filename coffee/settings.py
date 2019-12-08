c459eed3b0a757c83b75851226e2a59150225673
Kelvin Ndisi <kelvindisi@yahoo.com>
2019-12-08 08:29:48 +0300

    updated settings

--- coffee/settings.py
+++ coffee/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ia82_+_kwxtemqw@b9o!q@_%)_ci72)$%9stgcg1=(2k)aar!_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['yvonn3.herokuapp.com']
 
